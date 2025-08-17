e433fb1c0883b3e03c3d41dac53c78fbc8671844
kankaa16 <kankaoza1609@gmail.com>
2025-08-17 14:33:02 +0530

    final deployments

--- client/frontend/src/api.js
+++ client/frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_BASE = "https://ai-web-scraperr-vbtd.onrender.com";
 
-export const fetchWebsites = () => axios.get(`${API_BASE}/`);
-export const scrapeWebsite = (url) => axios.post(`${API_BASE}/scrape`, { url });
-export const updateWebsite = (id, updatedData) => axios.put(`${API_BASE}/${id}`, updatedData);
-export const deleteWebsite = (id) => axios.delete(`${API_BASE}/${id}`);
+export const fetchWebsites = () => axios.get(`${API_BASE}/api`);
+export const scrapeWebsite = (url) => axios.post(`${API_BASE}/api/scrape`, { url });
+export const updateWebsite = (id, updatedData) => axios.put(`${API_BASE}/api/${id}`, updatedData);
+export const deleteWebsite = (id) => axios.delete(`${API_BASE}/api/${id}`);
